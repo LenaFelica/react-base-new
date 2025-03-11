a1be76e9464e721a29f07da7cc0a891d22a4f39b
Lena Felica <leonatdg@mail.ru>
2025-03-11 16:57:07 +0300

    fix layout components

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
-import { Header } from './components/header';
 import { Footer } from './components/footer';
+import { Header } from './components/header';
 
 import styles from './layout.module.scss';
 
