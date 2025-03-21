760ed0ce771f452e32d7a7bff00ecf7a5de5d4e4
Lena Felica <leonatdg@mail.ru>
2025-03-21 11:58:38 +0300

    Для управления видимостью Tooltip использовала состояние useState + обработчики

--- src/components/tooltip/tooltip.jsx
+++ src/components/tooltip/tooltip.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
+
 import { TooltipIcon } from '@/assets/icons';
 
 import styles from './tooltip.module.scss';
 
 export const Tooltip = ({ text, icon: IconComponent = TooltipIcon }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const onMouseEnterHandler = () => {
+    setShowTooltip(true);
+  };
+
+  const onMouseLeaveHandler = () => {
+    setShowTooltip(false);
+  };
+
   return (
-    <span className={styles.tooltip}>
+    <span
+      className={styles.tooltip}
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
+    >
       <span className={styles.icon}>
         <IconComponent />
       </span>
-      <span className={styles.text}>{text}</span>
+      {showTooltip && <span className={styles.text}>{text}</span>}
     </span>
   );
 };
